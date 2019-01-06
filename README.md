efe21849c2d777cc16db8568a6ab69179f4613b5
gusaishuai <shilibusan@163.com>
2019-01-06 23:11:49 +0800

    excel操作基本开发完毕

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
 ### `Database design`
 
 CREATE TABLE `t_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `login_name` varchar(64) DEFAULT NULL COMMENT '登录名',
   `password` varchar(256) DEFAULT NULL COMMENT '密码',
   `nick` varchar(64) DEFAULT NULL COMMENT '昵称',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息';
+
+CREATE TABLE `t_menu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) DEFAULT NULL COMMENT '名称',
+  `route` varchar(64) DEFAULT NULL COMMENT '权限CODE',
+  `logo` varchar(64) DEFAULT NULL COMMENT 'logo',
+  `sort` int(11) DEFAULT NULL COMMENT '排序，从小到大',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='菜单信息';
+
+CREATE TABLE `t_user_menu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户ID',
+  `menu_id` int(11) DEFAULT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户菜单关系';
+
+CREATE TABLE `t_excel_filter_rule` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(128) DEFAULT NULL COMMENT '规则名称',
+  `user_id` int(11) DEFAULT NULL COMMENT '归属人员',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='EXCEL过滤规则';
+
+CREATE TABLE `t_excel_filter_rule_detail` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `rule_id` int(11) DEFAULT NULL COMMENT 't_excel_filter_rule表id',
+  `left_bracket` varchar(64) DEFAULT NULL COMMENT '左括号',
+  `col_num` int(11) DEFAULT NULL COMMENT '列数',
+  `matched` smallint(6) DEFAULT NULL COMMENT '是否满足 0-不满足 1-满足',
+  `regex` varchar(256) DEFAULT NULL COMMENT '值或正则表达式',
+  `right_bracket` varchar(64) DEFAULT NULL COMMENT '右括号',
+  `conj` varchar(8) DEFAULT NULL COMMENT '连接符 &-并且 |-或者',
+  PRIMARY KEY (`id`),
+  KEY `idx_rule_id` (`rule_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='EXCEL过滤规则详细';
 
 
 
--- src/main/java/com/imagespace/excel/action/ExcelFilterRuleUpdateAction.java
+++ src/main/java/com/imagespace/excel/action/ExcelFilterRuleUpdateAction.java
@@ -1,8 +1,55 @@
 package com.imagespace.excel.action;
 
+import com.imagespace.common.model.CallResult;
+import com.imagespace.common.model.ResultCode;
+import com.imagespace.common.service.ICallApi;
+import com.imagespace.common.util.ExceptionUtil;
+import com.imagespace.excel.service.ExcelService;
+import com.imagespace.user.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author gusaishuai
  * @since 2019/1/6
  */
-public class ExcelFilterRuleUpdateAction {
+@Slf4j
+@Service("excel.filterRuleUpdate")
+public class ExcelFilterRuleUpdateAction implements ICallApi {
+
+    @Autowired
+    private ExcelService excelService;
+
+    @Override
+    public CallResult exec(User _user, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //当前页数
+            String filterRuleName = request.getParameter("filterRuleName");
+            //代表以下6项内容的下标值
+            String[] exprRows = request.getParameterValues("exprRows[]");
+            //左括号，可多个，如：((，整体的表达式中，左括号必须和右括号的数量一致
+            String[] leftBracket = request.getParameterValues("leftBracket[]");
+            //列数
+            String[] colNum = request.getParameterValues("colNum[]");
+            //满足条件 1-满足 0-不满足
+            String[] matched = request.getParameterValues("matched[]");
+            //值或正则表达式
+            String[] regex = request.getParameterValues("regex[]");
+            //右括号，可多个，如：))，整体的表达式中，左括号必须和右括号的数量一致
+            String[] rightBracket = request.getParameterValues("rightBracket[]");
+            //连接符 &-并且 |-或者
+            String[] conj = request.getParameterValues("conj[]");
+            return new CallResult();
+        } catch (IllegalArgumentException e) {
+            return new CallResult(ResultCode.FAIL, e.getMessage());
+        } catch (Exception e) {
+            log.error("excel.filterRuleUpdate error", e);
+            return new CallResult(ResultCode.FAIL, ExceptionUtil.getExceptionTrace(e));
+        }
+    }
+
 }
